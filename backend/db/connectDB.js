9c346f845888b3a73cdae40aafb59bbddebe8462
Ishwar <ishwar.t@medkart.in>
2025-01-10 18:15:42 +0530

    DB connection converted from postgre to prisma

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,21 +1,7 @@
-import pg from "pg";
+import { PrismaClient } from "@prisma/client";
 
-const { Pool } = pg;
-const connectDB = async () => {
-  const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT, // default Postgres port
-    database: process.env.DB_NAME,
-  });
+const prisma = new PrismaClient({
+  log: ["query"],
+});
 
-  try {
-    await pool.connect();
-  } catch (err) {
-    console.error("Error connecting to the database:", err.stack);
-    throw err;
-  }
-};
-
-export default connectDB;
+export default prisma;
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 import dotenv from "dotenv";
-import connectDB from "./db/connectDB.js";
 import app from "./app.js";
 
 // Load env variables using import module
@@ -7,20 +6,23 @@ dotenv.config({
   path: ".env",
 });
 
-// Connect to the database and then start the server
-const startServer = async () => {
-  try {
-    await connectDB(); // Ensure DB is connected first
-    console.log("Database connected successfully");
+// // Connect to the database and then start the server
+// const startServer = async () => {
+//   try {
+//     await connectDB(); // Ensure DB is connected first
+//     console.log("Database connected successfully");
 
-    // Start the server after successful DB connection
-    app.listen(process.env.PORT || 8080, () => {
-      console.log(`Server is running on port ${process.env.PORT || 8080}`);
-    });
-  } catch (error) {
-    console.log("Postgres connection error:", error);
-    process.exit(1); // Exit the process if DB connection fails
-  }
-};
+//     // Start the server after successful DB connection
+//     app.listen(process.env.PORT || 8080, () => {
+//       console.log(`Server is running on port ${process.env.PORT || 8080}`);
+//     });
+//   } catch (error) {
+//     console.log("Postgres connection error:", error);
+//     process.exit(1); // Exit the process if DB connection fails
+//   }
+// };
 
-startServer();
+// startServer();
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Server is running on port ${process.env.PORT || 8080}`);
+});
